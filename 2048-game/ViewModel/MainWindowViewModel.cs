f18e8ad420d39da9274c406651cb053fa9870d8e
Maksim Filipiuk <maksim.filipiuk@gmail.com>
2019-01-16 00:48:39 +0200

    Revert "Created NewGame command"
    This reverts commit 0b1fed684007440940b2490b8f53d972c75f3cca.

--- 2048-game/ViewModel/MainWindowViewModel.cs
+++ 2048-game/ViewModel/MainWindowViewModel.cs
@@ -3,32 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using _2048_game.Infrastructure;
-using _2048_game.View;
 
 namespace _2048_game.ViewModel
 {
-    class MainWindowViewModel : ViewModelBase
+    class MainWindowViewModel
     {
-        RelayCommand newGameCommand;
-        public ICommand NewGameCommand
-        {
-            get
-            {
-                if(newGameCommand == null)
-                {
-                    newGameCommand = new RelayCommand(ExecuteNewGameCommand);
-                }
-
-                return newGameCommand;
-            }
-        }
-
-        private void ExecuteNewGameCommand(object obj)
-        {
-            GameWindow gameWindow = new GameWindow("LET`s START!");
-            gameWindow.ShowDialog();
-        }
     }
 }
