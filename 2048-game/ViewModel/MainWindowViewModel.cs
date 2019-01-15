4ab30ca3fd26f5d7ef1763c09a8f8222b9514243
Maksim Filipiuk <maksim.filipiuk@gmail.com>
2019-01-16 00:47:18 +0200

    Created NewGame command

--- 2048-game/ViewModel/MainWindowViewModel.cs
+++ 2048-game/ViewModel/MainWindowViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using _2048_game.Infrastructure;
+using _2048_game.View;
 
 namespace _2048_game.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : ViewModelBase
     {
+        RelayCommand newGameCommand;
+        public ICommand NewGameCommand
+        {
+            get
+            {
+                if(newGameCommand == null)
+                {
+                    newGameCommand = new RelayCommand(ExecuteNewGameCommand);
+                }
+
+                return newGameCommand;
+            }
+        }
+
+        private void ExecuteNewGameCommand(object obj)
+        {
+            GameWindow gameWindow = new GameWindow("LET`s START!");
+            gameWindow.ShowDialog();
+        }
     }
 }
